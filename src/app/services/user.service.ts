c76de7d07998987460eb28a7432d58529245d58f
Alex <al.gr.0017@gmail.com>
2023-08-14 12:23:46 +0300

    refactor: rules for adding errors

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Faker, SexType, en, pl, it, LocaleDefinition } from '@faker-js/faker';
 import { IUser } from '../interfaces/user.interface';
 import { ICountry } from '../interfaces/country.interface';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  constructor(private storageService: StorageService) {}
 
   countries: ICountry[] = [
     {
@@ -102,10 +103,74 @@ export class UserService {
     const locale = this.getCountryLocale(region);
 
     for (let index = 0; index < qt; index++) {
-      this.users.push(
-        this.createUser(locale, countryNames, this.switchFaker(region))
+      let user = this.createUser(
+        locale,
+        countryNames,
+        this.switchFaker(region)
       );
+      this.users.push(user);
     }
+  }
+
+  addErrorsToUsersData(errQt: number) {
+    const _users = this.users;
+    if (errQt > 0) {
+      const fields = ['fullname', 'address', 'phone'];
+
+      let errorField: keyof IUser | string, errorType: number, errorPos: number;
+
+      _users.map((user) => {
+        for (let index = 0; index < errQt; index++) {
+          const errorsRandom = this.generateRandomSequence(
+            this.storageService.getSeed(),
+            this.roundNumber(errQt, this.storageService.getSeed(), index) * 3
+          );
+
+          let prevField;
+          let prevType;
+          let prevPos;
+
+          for (let i = 0; i < errorsRandom.length; i += 3) {
+            if (i % 1 === 0) {
+              errorField =
+                fields[(errorsRandom[i] % user['address'].length) % 3];
+              if (prevField === errorField) {
+                errorField =
+                  fields[((errorsRandom[i] % user['address'].length) - 1) % 3];
+              } else {
+                prevField = errorField;
+              }
+            }
+            if (i % 2 === 0) {
+              errorType = (errorsRandom[i] % user['fullname'].length) % 3;
+              if (prevType === errorType) {
+                errorType =
+                  ((errorsRandom[i] % user['fullname'].length) - 1) % 3;
+              } else {
+                prevType = errorType;
+              }
+            }
+            if (i % 3 === 0) {
+              errorPos = errorField.length % errorsRandom[i];
+              if (prevPos === errorPos) {
+                errorPos = (errorField.length - 1) % errorsRandom[i];
+              } else {
+                prevPos = errorPos;
+              }
+            }
+            // console.log(errorField!, errorType!, errorPos!);
+
+            this.choiceErrorType(
+              user,
+              errorField! as keyof IUser,
+              errorType!,
+              errorPos!
+            );
+          }
+        }
+      });
+    }
+    // return _users
   }
 
   getRandomFromArray(item: any, qt: number, faker: Faker) {
@@ -196,64 +261,148 @@ export class UserService {
     countryNames: string[],
     faker: Faker
   ): string {
-    return `${faker.location.buildingNumber()}, ${direction} ${faker.location.street()}, ${secondaryAddress}, ${faker.location.city()}, ${this.generateCounty(
-      locale,
-      faker
-    )}, ${this.getRandomFromArray(
-      [`${state}`, `${state + '-'}`, `${state + ' '}`],
-      1,
-      faker
-    )}${this.getRandomFromArray(
-      [faker.location.zipCode('####'), faker.location.zipCode('#####')],
-      1,
-      faker
-    )}, ${this.choiseCountryNames(countryNames, faker)}`
-      .replaceAll(', , ', ', ')
-      .replaceAll(',,', ',');
+    if (locale[0].metadata?.code === 'en') {
+      return `${faker.location.buildingNumber()}, ${direction} ${faker.location.street()}, ${secondaryAddress}, ${faker.location.city()}, ${this.generateCounty(
+        locale,
+        faker
+      )}, ${this.getRandomFromArray(
+        [`${state}`, `${state + '-'}`, `${state + ' '}`],
+        1,
+        faker
+      )}${this.getRandomFromArray(
+        [faker.location.zipCode('####'), faker.location.zipCode('#####')],
+        1,
+        faker
+      )}, ${this.choiseCountryNames(countryNames, faker)}`
+        .replaceAll(', , ', ', ')
+        .replaceAll(',,', ',');
+    } else {
+      return `${direction} ${faker.location.street()}, ${faker.location.buildingNumber()}, ${secondaryAddress}, ${faker.location.city()}, ${this.generateCounty(
+        locale,
+        faker
+      )}, ${this.getRandomFromArray(
+        [`${state}`, `${state + '-'}`, `${state + ' '}`],
+        1,
+        faker
+      )}${this.getRandomFromArray(
+        [faker.location.zipCode('####'), faker.location.zipCode('#####')],
+        1,
+        faker
+      )}, ${this.choiseCountryNames(countryNames, faker)}`
+        .replaceAll(', , ', ', ')
+        .replaceAll(',,', ',');
+    }
   }
 
   generatePhoneNumber(faker: Faker): string {
     return faker.phone.number().replaceAll('.', '-').split('x')[0].trim();
   }
 
-  choiceErrorType(inputString: string): string {
-    const randomPosition = Math.floor(Math.random() * 2);
+  choiceErrorType(
+    user: IUser,
+    errorField: keyof IUser,
+    errorType: number,
+    errorPos: number
+  ): IUser {
     const functions = [
-      this.deleteOneRandomSymbolError(inputString),
-      this.addOneRandomSymbolError(inputString, 'w'),
-      this.swapRandomAdjacentCharacters(inputString),
+      this.deleteOneRandomSymbolError(user, errorField, errorPos),
+      this.addOneRandomSymbolError(user, errorField, errorPos),
+      this.swapRandomAdjacentCharacters(user, errorField, errorPos),
     ];
-    return functions[randomPosition];
+    return functions[errorType];
   }
 
-  deleteOneRandomSymbolError(inputString: string): string {
-    if (inputString.length === 0) {
-      throw new Error('Empty String');
+  deleteOneRandomSymbolError(
+    user: IUser,
+    errorField: keyof IUser,
+    errorPos: number
+  ): IUser {
+    if ((user[errorField] as string)[errorPos] === ' ') {
+      errorPos = errorPos + 1;
+    }
+    (user[errorField] as string) =
+      (user[errorField] as string).slice(0, errorPos) +
+      (user[errorField] as string).slice(errorPos + 1);
+    return user;
+  }
+
+  addOneRandomSymbolError(
+    user: IUser,
+    errorField: keyof IUser,
+    errorPos: number
+  ): IUser {
+    if ((user[errorField] as string)[errorPos] === ' ') {
+      errorPos = errorPos + 1;
+    }
+    let newChar;
+
+    if (
+      !!(user[errorField] as string).includes(
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+      )
+    ) {
+      newChar = '0123456789'.split('')[errorPos] || '';
+    } else {
+      newChar =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')[
+          errorPos
+        ] || 'r';
     }
 
-    const randomIndex = Math.floor(Math.random() * inputString.length);
-    return (
-      inputString.slice(0, randomIndex) + inputString.slice(randomIndex + 1)
-    );
-  }
+    if (errorPos < errorPos || errorPos > (user[errorField] as string).length) {
+      console.error('Index is out of range');
+    }
 
-  addOneRandomSymbolError(inputString: string, newChar: string): string {
-    const randomPosition = Math.floor(Math.random() * inputString.length);
-    return (
-      inputString.slice(0, randomPosition) +
+    (user[errorField] as string) =
+      (user[errorField] as string).slice(0, errorPos) +
       newChar +
-      inputString.slice(randomPosition)
-    );
+      (user[errorField] as string).slice(errorPos);
+
+    return user;
   }
 
-  swapRandomAdjacentCharacters(inputString: string): string {
-    const characters = inputString.split('');
-    const randomIndex = Math.floor(Math.random() * (characters.length - 1));
-    const temp = characters[randomIndex];
+  swapRandomAdjacentCharacters(
+    user: IUser,
+    errorField: keyof IUser,
+    errorPos: number
+  ): IUser {
+    const characters = (user[errorField] as string).split('');
+    if ((user[errorField] as string)[errorPos] === ' ') {
+      errorPos = errorPos + 1;
+    }
+    const temp = characters[errorPos];
 
-    characters[randomIndex] = characters[randomIndex + 1];
-    characters[randomIndex + 1] = temp;
+    characters[errorPos] = characters[errorPos + 1];
+    characters[errorPos + 1] = temp;
+    (user[errorField] as string) = characters.join('');
 
-    return characters.join('');
+    return user;
+  }
+
+  generateRandomSequence(seed: number, length: number): number[] {
+    const sequence: number[] = [];
+    let currentValue = seed;
+
+    for (let i = 0; i < length; i++) {
+      currentValue = (currentValue * 16807) % 2147483647;
+      sequence.push(currentValue);
+    }
+
+    return sequence;
+  }
+
+  roundNumber(errQt: number, seed: number, iteration: number): number {
+    const roundedNum = Math.round(errQt);
+    const adjustment = seed + iteration;
+
+    if (errQt % 1 === 0.5) {
+      if (adjustment % 2 === 0) {
+        return Math.floor(errQt);
+      } else {
+        return Math.ceil(errQt);
+      }
+    }
+
+    return roundedNum;
   }
 }
